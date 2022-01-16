c1013d803c5d89e74dfc37c465c6e8e3a64e15fd
Mike Laptop <mszulik@hotmail.com>
2022-01-16 13:12:06 +0000

    initial commit

---
+++ Graph/GraphHelper.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Graph;
+
+namespace GraphTutorial
+{
+    public class GraphHelper
+    {
+        private static DeviceCodeCredential tokenCredential;
+        private static GraphServiceClient graphClient;
+
+        public static void Initialize(string clientId,
+                                      string[] scopes,
+                                      Func<DeviceCodeInfo, CancellationToken, Task> callBack)
+        {
+            tokenCredential = new DeviceCodeCredential(callBack, "common", clientId);
+            graphClient = new GraphServiceClient(tokenCredential, scopes);
+        }
+
+        public static async Task<string> GetAccessTokenAsync(string[] scopes)
+        {
+            var context = new TokenRequestContext(scopes);
+            var response = await tokenCredential.GetTokenAsync(context);
+            return response.Token;
+        }
+
+        // <GetMeSnippet>
+        public static async Task<User> GetMeAsync()
+        {
+            try
+            {
+                // GET /me
+                return await graphClient.Me
+                    .Request()
+                    .Select(u => new {
+                        u.DisplayName,
+                        u.Id
+                    })
+                    .GetAsync();
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting signed-in user: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task<IDirectoryObjectGetMemberGroupsCollectionPage> GetMyGroupIDs()
+        {
+            try
+            {
+                // GET /me
+                var securityEnabledOnly = true;
+
+                return await graphClient.Me
+                    .GetMemberGroups(securityEnabledOnly)
+                    .Request()
+                    .PostAsync();
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting signed-in user groups: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task<IEnumerable<string>> GetMyGroupsNames(Microsoft.Graph.User user)
+        {
+            try
+            {
+                var page = await graphClient.Users["{"+user.Id+"}"].MemberOf
+                    .Request()
+                    .GetAsync();
+
+                var groupNames = new List<string>();
+
+                foreach (var item in page)
+                {
+                    if (item is Group)
+                    {
+                        groupNames.Add(((Group)item).DisplayName);
+                    }
+                }
+
+                return groupNames;
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting signed-in user groups: {ex.Message}");
+                return null;
+            }
+        }
+ 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace GraphTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".NET Core Graph Tutorial\n");
+
+            // <InitializationSnippet>
+            var appConfig = LoadAppSettings();
+
+            if (appConfig == null)
+            {
+                Console.WriteLine("Missing or invalid appsettings.json...exiting");
+                return;
+            }
+
+            var appId = appConfig["appId"];
+            var scopesString = appConfig["scopes"];
+            var scopes = scopesString.Split(';');
+
+            // Initialize Graph client
+            GraphHelper.Initialize(appId, scopes, (code, cancellation) => {
+                Console.WriteLine(code.Message);
+                return Task.FromResult(0);
+            });
+
+            // Get signed in user
+            var user = GraphHelper.GetMeAsync().Result;
+            Console.WriteLine($"Welcome {user.DisplayName}!\n");
+
+            int choice = -1;
+
+            while (choice != 0)
+            {
+                Console.WriteLine("Please choose one of the following options:");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Get User Groups IDs");
+                Console.WriteLine("2. Get User Groups Names");
+
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch (System.FormatException)
+                {
+                    // Set to invalid value
+                    choice = -1;
+                }
+
+                switch (choice)
+                {
+                    case 0:
+                        // Exit the program
+                        Console.WriteLine("Goodbye...");
+                        break;
+                    case 1:
+                        // List User Groups
+                        GetUserGroupsIDs();
+                        break;
+                    case 2:
+                        GetUserGroupsNames(user);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice! Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static void GetUserGroupsNames(Microsoft.Graph.User user)
+        {
+            var groupsPage = GraphHelper
+                .GetMyGroupsNames(user)
+                .Result;
+
+            Console.WriteLine("Groups:");
+
+            foreach (var groupId in groupsPage)
+            {
+                Console.WriteLine($"Group Name: {groupId}");
+            }
+        }
+
+        static void GetUserGroupsIDs()
+        {
+            var groupsPage = GraphHelper
+                .GetMyGroupIDs()
+                .Result;
+
+            Console.WriteLine("Groups:");
+
+            foreach (var groupId in groupsPage)
+            {
+                Console.WriteLine($"Group: {groupId}");
+            }
+        }
+
+        static IConfigurationRoot LoadAppSettings()
+        {
+            var appConfig = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            // Check for required settings
+            if (string.IsNullOrEmpty(appConfig["appId"]) ||
+                string.IsNullOrEmpty(appConfig["scopes"]))
+            {
+                return null;
+            }
+
+            return appConfig;
+        }
+    }
+}
